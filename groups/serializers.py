73fdd9211b14fecbc3a9de8927133549f09acb2b
shoon2430 <shoon2430@gmail.com>
2020-09-02 22:17:38 +0900

    그룹 참가 승인 기능 추가

--- groups/serializers.py
+++ groups/serializers.py
@@ -7,7 +7,8 @@ from users.serializers import UserBaseSerializer
 class GroupBaseSerializer(serializers.ModelSerializer):
     leader = UserBaseSerializer(read_only=True)
     members = UserBaseSerializer(many=True, read_only=True)
-    leader_id = serializers.IntegerField(write_only=True)
+    attends = UserBaseSerializer(many=True, read_only=True)
+    leader_id = serializers.CharField(write_only=True)
 
     class Meta:
         model = Group
@@ -17,12 +18,13 @@ class GroupBaseSerializer(serializers.ModelSerializer):
             "discription",
             "leader",
             "leader_id",
-            "members",
             "time",
+            "members",
+            "attends",
         )
 
     def create(self, validated_data):
-        user = User.objects.get(id=validated_data["leader_id"])
+        user = User.objects.get(username=validated_data["leader_id"])
         group = Group.objects.create(
             category=validated_data["category"],
             title=validated_data["title"],
@@ -46,5 +48,25 @@ class attendToGroupSerializer(serializers.ModelSerializer):
         fields = ("userId",)
 
     def create(self, validated_data):
-        self.group.members.add(User.objects.get(username=validated_data["userId"]))
+        self.group.attends.add(User.objects.get(username=validated_data["userId"]))
+        return self.group
+
+
+# 그룹 참가 승인
+class confirmToGroupSerializer(serializers.ModelSerializer):
+    userId = serializers.CharField(required=False)
+
+    def __init__(self, group, *args, **kwargs):
+        super(confirmToGroupSerializer, self).__init__(*args, **kwargs)
+        self.group = group
+
+    class Meta:
+        model = Group
+        fields = ("userId",)
+
+    def create(self, validated_data):
+
+        user = User.objects.get(username=validated_data["userId"])
+        self.group.attends.remove(user)
+        self.group.members.add(user)
         return self.group
--- groups/tests.py
+++ groups/tests.py
@@ -13,21 +13,21 @@ class GroupTestCase(APITestCase):
     def setUp(self):
 
         group = {
-            "category": "local",
+            "category": "cate1",
             "title": "localhost1",
             "discription": "Hello my name ist local",
-            "leader_id": "1",
+            "leader_id": "local",
             "time": "1",
         }
 
         user1 = {
-            "username": "local2",
+            "username": "local",
             "email": "test@local.host",
             "password": "1234",
-            "name": "jsh",
+            "name": "hoon",
         }
         user2 = {
-            "username": "local3",
+            "username": "callo",
             "email": "test@local.host",
             "password": "1234",
             "name": "jsh",
@@ -37,22 +37,47 @@ class GroupTestCase(APITestCase):
         response = self.client.post(url, user1, format="json")
         response = self.client.post(url, user2, format="json")
 
-        url = reverse("groups:all-list")
+        url = reverse("groups:list")
         data = group
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    # 그룹 생성테스트
     def test_create_user(self):
         self.assertEqual(Group.objects.count(), 1)
         self.assertEqual(Group.objects.get().title, "localhost1")
 
+    # 그룹 참여 테스트
     def test_join_group(self):
         user = User.objects.get(id=2)
 
         self.client.force_authenticate(user)
-        url = reverse("groups:update", args=[1])
+        url = reverse("groups:attend", args=[1])
         # data = {"userId": "local2"}
         # response = self.client.put(url, data, format="json")
 
         data = {"userId": user.username}
         response = self.client.put(url, data, format="json")
+
+        self.assertIn(user, Group.objects.get().attends.all())
+
+    # 그룹 승인 테스트
+    def test_confirm_group(self):
+        leader = User.objects.get(id=1)
+        member = User.objects.get(id=2)
+        self.client.force_authenticate(member)
+
+        url = reverse("groups:attend", args=[1])
+        data = {"userId": member.username}
+        response = self.client.put(url, data, format="json")
+
+        self.assertIn(member, Group.objects.get().attends.all())
+        self.assertNotIn(member, Group.objects.get().members.all())
+
+        self.client.force_authenticate(leader)
+        url = reverse("groups:confirm", args=[1])
+        data = {"userId": member.username}
+        response = self.client.put(url, data, format="json")
+
+        self.assertIn(member, Group.objects.get().members.all())
+        self.assertNotIn(member, Group.objects.get().attends.all())
