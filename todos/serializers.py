3dbcbb5cce5ca8fb51fb2ee385ca11e7ef72f1ed
shoon2430 <shoon2430@gmail.com>
2020-09-09 21:00:50 +0900

    subject CRUD기능 추가 및 테스트

--- todos/serializers.py
+++ todos/serializers.py
@@ -7,27 +7,36 @@ from .models import Subject, Todo
 from users.serializers import userSimpleInfoSerializer
 
 
+class TodoSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ("subject_id", "time", "title", "writer", "progress")
+
+
 class SubjectSimpleSerializer(serializers.ModelSerializer):
+    todos = TodoSimpleSerializer(many=True)
+
     class Meta:
         model = Subject
-        fields = (
-            "group_id",
-            "time",
-            "title",
-            "writer",
-        )
+        fields = ("group_id", "time", "title", "writer", "todos")
+
+
+class groupSubjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ("group_id", "time", "title", "writer", "todos")
 
 
 class SubjectBaseSerializer(serializers.ModelSerializer):
 
     group_id = serializers.UUIDField(read_only=True)
     time = serializers.IntegerField(read_only=True)
-    writer = userSimpleInfoSerializer()
+    writer = userSimpleInfoSerializer(read_only=True)
 
     def __init__(self, group, leader, *args, **kwargs):
         super(SubjectBaseSerializer, self).__init__(*args, **kwargs)
         self.group = group
-        self.user = leader
+        self.leader = leader
 
     class Meta:
         model = Subject
@@ -46,4 +55,10 @@ class SubjectBaseSerializer(serializers.ModelSerializer):
             group_id=self.group,
             writer=self.leader,
         )
-        return super().create(validated_data)
+        return subject
+
+
+class SubjectDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = ("title",)
--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,83 @@
-from django.test import TestCase
+import json
+from pprint import pprint
+from django.urls import reverse
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from rest_framework import status
+
+from .models import Subject, Todo
+from groups.models import Group
+from users.models import User
+
+from users.tests import userCreateSetUp
+
+
+class SubejectCreateTestCase(userCreateSetUp):
+    def setUp(self):
+        super().setUp()
+        data = {
+            "category": "test",
+            "title": "localhostGroup",
+            "discription": "Hello my name ist local",
+        }
+
+        url = reverse("groups:groups")
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.jwt_token_1}")
+        response = self.client.post(url, data, format="json")
+
+        self.group = Group.objects.get()
+
+        data = {"title": "subjectTest"}
+        url = reverse("todos:subjects", args=[self.group.pk])
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.jwt_token_1}")
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(json.loads(response.content)["title"], "subjectTest")
+
+    # 서브젝트 수정
+    def test_update_subject(self):
+        group = self.group
+        subject = Subject.objects.get()
+        leader = User.objects.get(username=self.user1["username"])
+        member = User.objects.get(username=self.user2["username"])
+
+        data = {"title": "Change_subjectTest"}
+        url = reverse(
+            "todos:subject-detail",
+            kwargs={"group_pk": group.pk, "subject_pk": subject.pk},
+        )
+
+        # 해당그룹의 리더가 수정시
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.jwt_token_1}")
+        response = self.client.patch(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json.loads(response.content)["title"], "Change_subjectTest")
+
+        # 그룹의 리더가 아닌 사람이 수정시
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.jwt_token_2}")
+        response = self.client.patch(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(json.loads(response.content)["detail"], "그룹장만 승인이 가능합니다.")
+
+    # 서브젝트 삭제
+    def test_delete_subject(self):
+        group = self.group
+        subject = Subject.objects.get()
+        leader = User.objects.get(username=self.user1["username"])
+        member = User.objects.get(username=self.user2["username"])
+
+        url = reverse(
+            "todos:subject-detail",
+            kwargs={"group_pk": group.pk, "subject_pk": subject.pk},
+        )
+
+        # 그룹의 리더가 아닌 사람이 삭제시
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.jwt_token_2}")
+        response = self.client.delete(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # 그룹의 리더가 삭제시
+        self.client.credentials(HTTP_AUTHORIZATION=f"JWT {self.jwt_token_1}")
+        response = self.client.delete(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
